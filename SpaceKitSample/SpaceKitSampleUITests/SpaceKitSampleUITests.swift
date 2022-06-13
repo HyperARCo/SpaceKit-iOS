fad49e4b0951806fb3e81a17ce62765c52fc8b54
Simon Goldring <sbjgoldring@gmail.com>
2022-06-13 17:42:32 +0100

    Remove UI launch tests to save CI minutes
    (XCT) Skip currently unwanted testLaunchPerformance test
    SC-3453

--- SpaceKitSample/SpaceKitSampleUITests/SpaceKitSampleUITests.swift
+++ SpaceKitSample/SpaceKitSampleUITests/SpaceKitSampleUITests.swift
@@ -25,6 +25,8 @@ class SpaceKitSampleUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
+		throw XCTSkip()
+		
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
