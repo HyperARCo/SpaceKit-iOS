4200f72b7e1866f4d8b515342031687229c1c5a2
dentreality <simon@dentreality.com>
2022-08-03 22:50:11 +0000

    See https://github.com/DentReality/iOS-Hyper/commit/788c97cf60300fc99bf2859401403ce7221bcbb0 from refs/heads/dev

---
+++ SpaceKitSample/SpaceKitSample/List/ListManager.swift
@@ -0,0 +1,66 @@
+//
+// Copyright © 2022 Dent Reality. All rights reserved.
+//
+
+import SpaceKit
+
+class ListManager {
+	private let products: [Product]
+	
+	var listItems = [String]()
+	var productItems = [String]()
+	
+	var updateListView: (() -> ())?
+	
+	var spaceKitContext: SpaceKit.Context?
+	
+	init(products: [Product]) {
+		self.products = products
+		updateProductItems()
+	}
+	
+	func product(with identifer: String) -> Product? {
+		products.first(where: { $0.upc == identifer })
+	}
+	
+	func removeListItem(at index: Int) {
+		listItems.remove(at: index)
+		updateListView?()
+		
+		let destinations = destinations(for: listItems)
+		spaceKitContext?.setDestinations(destinations)
+	}
+	
+	func addProduct(at index: Int) {
+		let productItem = productItems.remove(at: index)
+		updateListView?()
+
+		let newListItems = listItems + [productItem]
+		let destinations = destinations(for: newListItems)
+		spaceKitContext?.setDestinations(destinations)
+	}
+	
+	private func destinations(for identifiers: [String]) -> [SpaceKit.Destination] {
+		identifiers.compactMap { identifier in
+			guard let product = product(with: identifier) else { return nil }
+			return Destination(identifier: identifier, displayName: product.name, icon: UIImage(named: product.icon))
+		}
+	}
+	
+	private func updateProductItems() {
+		self.productItems = products
+			.filter { product in
+				!listItems.contains(product.upc)
+			}
+			.sorted()
+			.map(\.upc)
+	}
+}
+
+extension ListManager: SpaceKit.SpaceKitListDelegate {
+	func spaceKitContext(_ context: Context, didUpdateOrderedDestinations orderedDestinations: [Destination]) {
+		self.listItems = orderedDestinations.map(\.identifier)
+		updateProductItems()
+		updateListView?()
+	}
+}
