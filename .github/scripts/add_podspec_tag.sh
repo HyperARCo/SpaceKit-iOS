9b56198cd5d68a476eb08677305d6a7fb904d5f6
Chris Rivers <chris@dentreality.com>
2022-11-02 14:35:01 +0000

    Add workflow to update Cocoapod version

---
+++ .github/scripts/add_podspec_tag.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -eo pipefail
+
+TAG=$(grep -E 'spec.version *= "[0-9]+.[0-9]+.[0-9]+"' SpaceKit.podspec | grep -oE '[0-9]+.[0-9]+.[0-9]+')
+
+echo "Adding tag:" $TAG
+
+git tag $TAG
