2d8f15afd64e2fd571882dbb7e2f6ca98e8e41db
Chris Rivers <chris@dentreality.com>
2022-12-05 14:30:15 +0000

    Push docs after publishing

--- .github/scripts/publish-docs.sh
+++ .github/scripts/publish-docs.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+set -eo pipefail
+
 $(xcrun --find docc) process-archive \
 transform-for-static-hosting SpaceKit.doccarchive \
 --output-path $SPACEKIT_DOCS_DIRECTORY \
 --hosting-base-path $SPACEKIT_HOSTING_BASE_PATH
+
+git commit -m "Update docs"
+git push origin main
